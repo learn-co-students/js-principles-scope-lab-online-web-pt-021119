73570543370822de508d4fb3b10a55030015bebc
chilbert <chase@chasehilbert.com>
2019-08-27 13:09:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+const leastFavoriteCustomer = "caleb"
+
+function upperCaseCustomerName() {
+  name = customerName.toUpperCase()
+  customerName = name
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"
+
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "chase"
+
+}
